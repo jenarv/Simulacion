0324e5b972ca7639362ae13dea4b4f2b9f2576ab
Jennifer Miriam Arellano Villaseñor <159722274+jenarv@users.noreply.github.com>
2024-06-06 15:41:13 -0600

    Update GeneticAgent.java

--- HandsOn5_MultiAgent/src/GeneticAgent.java
+++ HandsOn5_MultiAgent/src/GeneticAgent.java
@@ -80,43 +80,6 @@ public class GeneticAgent extends Agent {
         }
     }
 
-    public class Chromosome {
-        private double beta0;
-        private double beta1;
-        private double fitness;
-
-        public Chromosome(double beta0, double beta1) {
-            this.beta0 = beta0;
-            this.beta1 = beta1;
-            this.fitness = 0.0;
-        }
-
-        public double getBeta0() {
-            return beta0;
-        }
-
-        public void setBeta0(double beta0) {
-            this.beta0 = beta0;
-        }
-
-        public double getBeta1() {
-            return beta1;
-        }
-
-        public void setBeta1(double beta1) {
-            this.beta1 = beta1;
-        }
-
-        public double getFitness() {
-            return fitness;
-        }
-
-        public void setFitness(double fitness) {
-            this.fitness = fitness;
-        }
-    }
-
-
 }
 
 
