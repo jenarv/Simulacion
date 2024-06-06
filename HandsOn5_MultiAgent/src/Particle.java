9728d9f51d06514e94a3d6f214ff8591e2bd6afb
Jennifer Miriam Arellano Villaseñor <159722274+jenarv@users.noreply.github.com>
2024-06-06 15:44:04 -0600

    Update Particle.java

--- HandsOn5_MultiAgent/src/Particle.java
+++ HandsOn5_MultiAgent/src/Particle.java
@@ -1,1 +1,13 @@
+ public class Particle {
+        double[] position;
+        double[] velocity;
+        double[] bestPosition;
+        double bestFitness;
 
+        public Particle(int dimensions) {
+            position = new double[dimensions];
+            velocity = new double[dimensions];
+            bestPosition = new double[dimensions];
+            bestFitness = Double.MAX_VALUE;
+        }
+    }
